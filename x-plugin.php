083ab7dd2b0f0695bf4e5a830659aa27d206d7a4
LordSylt <lordsylt@DESKTOP-RGF6P38.localdomain>
2026-04-15 19:33:54 +0200

    Initial plugin commit

---
+++ x-plugin.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Plugin Name:       X Plugin
+ * Description:       Example block scaffolded with Create Block tool.
+ * Version:           0.1.0
+ * Requires at least: 6.8
+ * Requires PHP:      7.4
+ * Author:            The WordPress Contributors
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       x-plugin
+ *
+ * @package CreateBlock
+ */
+
+
+
+//PHP handles backend logic and wordpress hooks
+
