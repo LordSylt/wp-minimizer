ddaf4f90f7b9a5f5a45e04090975bdfd06196071
gustav1624 <gurramollerberg@gmail.com>
2026-05-05 16:34:31 +0200

    formatted php file

--- plugin.php
+++ plugin.php
@@ -13,43 +13,42 @@
  * @package CreateBlock
  */
 
-
 //Debugging func
-function logToConsole($message) {
-	echo "<script>console.log('" . json_encode( $message ) ."');</script>";
+function logToConsole($message)
+{
+    echo "<script>console.log('" . json_encode($message) . "');</script>";
 }
 
+$slim = [
+    "core/paragraph",
+    "core/heading",
+    "core/list",
+    "core/table",
+    "core/image",
+    "core/file",
+    "core/video",
+    "core/accordion",
+    "core/buttons",
+    "core/site-logo",
+];
 
-$slim = array(
-			'core/paragraph',
-			'core/heading',
-			'core/list',
-			'core/table',
-			'core/image',
-			'core/file',
-			'core/video',
-			'core/accordion',
-			'core/buttons',
-            'core/site-logo',
-		);
-
-$medium = array(
-			'core/paragraph',
-			'core/heading',
-			'core/list',
-			'core/table',
-			'core/image',
-			'core/file',
-			'core/video',
-			'core/accordion',
-			'core/buttons',
-            'core/site-logo',
-            'core/quote',
-            'core/details',
-            'core/math',
-            'core/columns',
-            'core/embed',
-		);
+$medium = [
+    "core/paragraph",
+    "core/heading",
+    "core/list",
+    "core/table",
+    "core/image",
+    "core/file",
+    "core/video",
+    "core/accordion",
+    "core/buttons",
+    "core/site-logo",
+    "core/quote",
+    "core/details",
+    "core/math",
+    "core/columns",
+    "core/embed",
+];
 
 $full = true;
 
@@ -57,97 +56,107 @@ $full = true;
  * Initializes necessary database columns on startup if needed
  * @return void
  */
-function add_current_editor_state_field() {
-	global $wpdb;
-	$row = $wpdb->get_results("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
+function add_current_editor_state_field()
+{
+    global $wpdb;
+    $row = $wpdb->get_results("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS
 								WHERE table_name = 'wp_users' AND column_name = 'editor_state'");
-	if (empty($row)) {
-		$wpdb->query("ALTER TABLE wp_users ADD editor_state VARCHAR(20) NOT NULL DEFAULT 'slim'");
-	}
+    if (empty($row)) {
+        $wpdb->query(
+            "ALTER TABLE wp_users ADD editor_state VARCHAR(20) NOT NULL DEFAULT 'slim'"
+        );
+    }
 }
 
-add_action("init","add_current_editor_state_field");
+add_action("init", "add_current_editor_state_field");
 
-function sidebar_plugin_register() {
+function sidebar_plugin_register()
+{
     wp_register_script(
-        'minimizer-sidebar',
-        plugins_url( 'build/index.js', __FILE__ ),
-        array( 'wp-plugins', 'wp-edit-post', 'react', 'wp-components' )
+        "minimizer-sidebar",
+        plugins_url("build/index.js", __FILE__),
+        ["wp-plugins", "wp-edit-post", "react", "wp-components"]
     );
 }
-add_action( 'init', 'sidebar_plugin_register' );
+add_action("init", "sidebar_plugin_register");
 
-function sidebar_plugin_script_enqueue() {
-	global $wpdb;
-    wp_enqueue_script( 'minimizer-sidebar' );
+function sidebar_plugin_script_enqueue()
+{
+    global $wpdb;
+    wp_enqueue_script("minimizer-sidebar");
 
-	$user_id = get_current_user_id();
-	$preset = $wpdb->get_results("SELECT editor_state FROM wp_users WHERE ID = $user_id");
-	$preset = $preset[0]->editor_state;
+    $user_id = get_current_user_id();
+    $preset = $wpdb->get_results(
+        "SELECT editor_state FROM wp_users WHERE ID = $user_id"
+    );
+    $preset = $preset[0]->editor_state;
 
-	wp_localize_script('minimizer-sidebar', 'minimizer', [
-        'ajaxUrl' 	=> admin_url('admin-ajax.php'),
-        'nonce'   	=> wp_create_nonce('wp_minimizer_nonce'),
-		'preset'	=> $preset,
+    wp_localize_script("minimizer-sidebar", "minimizer", [
+        "ajaxUrl" => admin_url("admin-ajax.php"),
+        "nonce" => wp_create_nonce("wp_minimizer_nonce"),
+        "preset" => $preset,
     ]);
 }
-add_action( 'enqueue_block_editor_assets', 'sidebar_plugin_script_enqueue' );
-
+add_action("enqueue_block_editor_assets", "sidebar_plugin_script_enqueue");
 
 /**
  * Adds an action hook to call from React, checks permissions then updates loaded preset
  */
-add_action('wp_ajax_wp_minimizer_set_preset', function() {
-	global $wpdb;
-	check_ajax_referer('wp_minimizer_nonce', 'nonce');
-	if (!current_user_can('edit_posts')) {
-		wp_send_json_error('Unauthorized', 403);
-	}
+add_action("wp_ajax_wp_minimizer_set_preset", function () {
+    global $wpdb;
+    check_ajax_referer("wp_minimizer_nonce", "nonce");
+    if (!current_user_can("edit_posts")) {
+        wp_send_json_error("Unauthorized", 403);
+    }
 
-	$user_id = get_current_user_id();
-	$preset = sanitize_text_field($_POST['value'] ?? '');
-	if (!$user_id || !in_array($preset, ['slim', 'medium', 'full'], true)) {
-		wp_send_json_error('Invalid preset', 400);
-	}
+    $user_id = get_current_user_id();
+    $preset = sanitize_text_field($_POST["value"] ?? "");
+    if (!$user_id || !in_array($preset, ["slim", "medium", "full"], true)) {
+        wp_send_json_error("Invalid preset", 400);
+    }
 
-	$result = $wpdb->update("wp_users", array('editor_state'=>$preset), array('ID'=>$user_id));
-	if (!$result) {
-		wp_send_json_error('Database failure', 400);
-	}
-	wp_send_json_success();
+    $result = $wpdb->update(
+        "wp_users",
+        ["editor_state" => $preset],
+        ["ID" => $user_id]
+    );
+    if (!$result) {
+        wp_send_json_error("Database failure", 400);
+    }
+    wp_send_json_success();
 });
-
-
 
 /**
  * Fetches preset from db and applies it
  * @param mixed $block_editor_context
  * @param mixed $editor_context
  */
-function wpdocs_allowed_block_types($block_editor_context, $editor_context) {
-	global $slim, $medium, $full, $wpdb;
-	if (! empty($editor_context->post)) {
+function wpdocs_allowed_block_types($block_editor_context, $editor_context)
+{
+    global $slim, $medium, $full, $wpdb;
+    if (!empty($editor_context->post)) {
+        #Fetches previously stored preset.
+        $user_id = get_current_user_id();
+        $preset = $wpdb->get_results(
+            "SELECT editor_state FROM wp_users WHERE ID = $user_id"
+        );
+        if (!$preset) {
+            return $block_editor_context;
+        }
+        $preset = $preset[0]->editor_state;
 
-		#Fetches previously stored preset.		
-		$user_id = get_current_user_id();
-	    $preset = $wpdb->get_results("SELECT editor_state FROM wp_users WHERE ID = $user_id");
-		if (!$preset) {
-			return $block_editor_context;
-		}
-		$preset = $preset[0]->editor_state;
-
-		switch ($preset) {
-			case 'slim':
-				return $slim;
-			case 'medium':
-				return $medium;
-			case 'full':
-				return $full;
-			default:
-				return $block_editor_context;
-		}
-	}
-	return $block_editor_context;
+        switch ($preset) {
+            case "slim":
+                return $slim;
+            case "medium":
+                return $medium;
+            case "full":
+                return $full;
+            default:
+                return $block_editor_context;
+        }
+    }
+    return $block_editor_context;
 }
 
-add_filter( 'allowed_block_types_all', 'wpdocs_allowed_block_types', 10, 2 );
+add_filter("allowed_block_types_all", "wpdocs_allowed_block_types", 10, 2);
